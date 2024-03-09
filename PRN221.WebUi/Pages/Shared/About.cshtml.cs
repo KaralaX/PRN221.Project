1a34a9c76885c0a0ad28832b307420417cd07cdf
thientsche170402 <thientsche170402@fpt.edu.vn>
2024-03-09 13:25:50 +0700

    add layout

---
+++ PRN221.WebUi/Pages/Shared/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN221.WebUi.Pages.Shared
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
