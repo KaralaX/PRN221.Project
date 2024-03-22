2e6956632b5da99d904525240510b933c27c69be
NguyenMinh <minhngquangg@gmail.com>
2024-03-22 17:02:54 +0700

    Finish configuration for online payment function

---
+++ PRN221.Project.Infrastructure/VnPay/Services/IVnPayService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using PRN221.Project.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN221.Project.Infrastructure.Services
+{
+    public interface IVnPayService
+    {
+        string CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+        PaymentResponseModel PaymentExecute(IQueryCollection collections);
+    }
+}
